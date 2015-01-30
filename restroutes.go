f1be252e69a24122917118d1f207c8977e413fda
Jack Eisenberg <jack@Jacks-MacBook-Pro-2.local>
2015-01-30 10:19:42 -0600

    updates

--- restroutes.go
+++ restroutes.go
@@ -1,6 +1,7 @@
 package restroutes
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
 	"net/http"
 	"reflect"
@@ -8,6 +9,10 @@ import (
 
 type handlerInterface interface {
 	HandleFunc(string, func(http.ResponseWriter, *http.Request)) *mux.Route
+}
+
+type initializeInterface interface {
+	Initialize()
 }
 
 type Route struct {
@@ -24,5 +29,20 @@ func Register(m handlerInterface, routes Routes) {
 		methodIface := s.Interface()
 		method := methodIface.(func(w http.ResponseWriter, r *http.Request))
 		m.HandleFunc(k, method).Methods(v.RequestMethod)
+	}
+}
+
+func RegisterGin(e *gin.Engine, routes Routes) {
+	for k, v := range routes {
+		r := reflect.ValueOf(v.Receiver)
+		if _, ok := r.Interface().(initializeInterface); ok {
+			r.MethodByName("Initialize").Call([]reflect.Value{})
+		}
+		s := reflect.ValueOf(v.Receiver).MethodByName(v.Method)
+		methodIface := s.Interface()
+		method := methodIface.(func(c *gin.Context))
+		f := reflect.ValueOf(e).MethodByName(v.RequestMethod)
+		arg := []reflect.Value{reflect.ValueOf(k), reflect.ValueOf(method)}
+		f.Call(arg)
 	}
 }
